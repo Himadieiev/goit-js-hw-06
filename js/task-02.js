23b31605c2deffc8ab1cf0a42ed990d03abbe021
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-07 12:07:18 +0200

    add

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('ul');
+
+const elements = ingredients.map(ingredient => {
+  const elementOfIngredients = document.createElement('li');
+  elementOfIngredients.textContent = ingredient;
+  elementOfIngredients.classList.add('item');
+  
+  return elementOfIngredients;
+})
+
+console.log(elements);
+
+list.append(...elements);
