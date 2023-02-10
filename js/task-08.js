9420595b73a1f90dd0796b550bea5e3f40d40a85
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-10 16:25:48 +0200

    add

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
