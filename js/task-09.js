81f9575fefa52064521ca93f13f5d6685690fc48
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-11 13:39:24 +0200

    add

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+const span = document.querySelector('span');
+
+const changeBgColorBody = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
+
+button.addEventListener('click', changeBgColorBody);
